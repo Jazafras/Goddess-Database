7a410eb1c1e6db81cdacbc98c4fe7b3cba1df930
Monte C Roden <monte.roden@wsu.edu>
2017-12-08 04:33:37 -0800

    Basic funcitonality for the front end is done

--- goddesses.py
+++ goddesses.py
@@ -1,5 +1,6 @@
 import os
 import json
+import ast
 
 from bs4 import BeautifulSoup as soup
 import whoosh.index
@@ -34,6 +35,14 @@ def search():
 @app.route("/goddess/", methods=['POST', 'GET'])
 def goddess():
     goddess = json.load(open("data/" + request.args["pageid"] + ".json"))
+    # Stackoverflow on "Convert list representation of list to String"
+    related = []
+    related_indices = goddess["similar"]
+    related_indices = ast.literal_eval(related_indices)
+    related_indices = [n.strip() for n in related_indices]
+    for index in related_indices:
+        related_goddess = json.load(open("data/" + index + ".json"))
+        related.append(related_goddess)
     # Remove all the h2's and li's.
     # The h2's are often empty, the li's are just ugly.
     my_soup = soup(goddess["extract"], "lxml")
@@ -42,7 +51,7 @@ def goddess():
     for tags in my_soup.find_all('li'):
         tags.extract()
     goddess["extract"] = my_soup
-    return render_template('goddess_page.html', goddess=goddess)
+    return render_template('goddess_page.html', goddess=goddess, related=related)
 
 if __name__ == "__main__":
     app.secret_key = "ASDFASj~~8888???,,,/"
